ee1ff7e9546c25fa152764c7191a3fb6b8aa85ad
kirtanchandak <themarketingkirtan@gmail.com>
2023-09-09 01:25:28 +0530

    what we do section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar.jsx";
+import WhatWeDo from "./components/WhatWeDo";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <WhatWeDo />
     </>
   );
 }
---
+++ src/components/WhatWeDo.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import event from "../assets/whatwedo-icons/events.png";
+import opensource from "../assets/whatwedo-icons/open-source.png";
+import technical from "../assets/whatwedo-icons/projects.png";
+
+function WhatWeDo() {
+  return (
+    <div className="flex justify-center items-center px-5 pt-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
+        <div className="bg-[#F6F6F7] p-2 rounded-lg hover:bg-gray-200">
+          <div className="p-3">
+            <img
+              src={event}
+              alt=""
+              className="bg-[#e3e3e5] inline-block p-2 rounded-md"
+            />
+            <div className="mt-4">
+              <h1 className="font-semibold text-xl">Events</h1>
+              <p className="text-sm mt-2">
+                Hot Module Replacement (HMR) that stays fast <br />
+                regardless of app size.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#F6F6F7] p-2 rounded-lg hover:bg-gray-200">
+          <div className="p-3">
+            <img
+              src={opensource}
+              alt=""
+              className="bg-[#e3e3e5] inline-block p-2 rounded-md"
+            />
+            <div className="mt-4">
+              <h1 className="font-semibold text-xl">Open Source</h1>
+              <p className="text-sm mt-2">
+                Hot Module Replacement (HMR) that stays fast <br />
+                regardless of app size.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#F6F6F7] p-2 rounded-lg hover:bg-gray-200">
+          <div className="p-3">
+            <img
+              src={technical}
+              alt=""
+              className="bg-[#e3e3e5] inline-block p-2 rounded-md"
+            />
+            <div className="mt-4">
+              <h1 className="font-semibold text-xl">Technical Projects</h1>
+              <p className="text-sm mt-2">
+                Hot Module Replacement (HMR) that stays fast <br />
+                regardless of app size.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WhatWeDo;
