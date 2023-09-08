f4bd98deb1b3a2683b0342140f17ee7216df0e2b
kirtanchandak <themarketingkirtan@gmail.com>
2023-09-08 13:16:06 +0530

    website init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>MLSC Website</h1>
+        <p>Jai Shree Ram!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
