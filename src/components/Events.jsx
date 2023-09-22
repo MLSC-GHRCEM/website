dbb4d582316e1772c7f51d3f302e14dc246f8188
kirtanchandak <themarketingkirtan@gmail.com>
2023-09-22 17:54:28 +0530

    events page enhancement

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import {eventdata} from './content'
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { eventdata } from "./content";
 function Events() {
   return (
     <>
-    <Navbar/>
-    <div className="pt-20" id="about">
-   <h1 className="text-center font-bold text-4xl pb-20">Events</h1>
-   <div class=" px-20 text-center space-y-4 md:space-y-0  grid grid-cols-1 md:grid-cols-3 gap-20  w-full pb-10">
-    {eventdata.map((content)=>(
-      <div className="bg-[#F5F7FB]   shadow-[0_3px_10px_rgb(0,0,0,0.2)]  cursor-pointer transform transition-transform ">
-    
-       <img className='block m-auto ' src={content.image}/>     
-  <p className="py-1 font-bold text-md lg:text-xl text-primary">
-                {content.eventname}
-              </p>
+      <Navbar />
+      <div className="pt-20" id="about">
+        <h1 className="text-center font-bold text-4xl pb-20">Events</h1>
+        <div class="px-8 md:px-20 text-center md:space-y-0 grid grid-cols-1 md:grid-cols-3 md:gap-20 gap-12 w-full pb-10">
+          {eventdata.map((content) => (
+            <div className="bg-[#F5F7FB] shadow-[0_3px_10px_rgb(0,0,0,0.2)]  cursor-pointer transform transition-transform p-4">
+              <div>
+                <img className="block m-auto w-44" src={content.image} />
+              </div>
+              <div>
+                <p className="font-bold text-2xl lg:text-xl text-primary pt-6">
+                  {content.title}
+                </p>
+                <p>{content.description}</p>
+              </div>
+              <div>
+                <button className="p-2 bg-primary rounded-full text-white mt-2">
+                  Register Now
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-    ))};
-   </div>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Events
-
+export default Events;
--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-
-
-
-export const  eventdata = [
-    {
- id:1,
- eventname :"Github",
- image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBICPHJ86kQ-y6p2-Z9K_ibEbYmVmo6Eda-w&usqp=CAU",
-    },
-    {
-  id:2,
- eventname :"Hacktoberfest",
- image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBICPHJ86kQ-y6p2-Z9K_ibEbYmVmo6Eda-w&usqp=CAU",
-    },
-    {
-  id:3,
- eventname :"Hacktoberfest",
- image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBICPHJ86kQ-y6p2-Z9K_ibEbYmVmo6Eda-w&usqp=CAU",
-          },
-]
-
-
+export const eventdata = [
+  {
+    id: 1,
+    title: "Git & Github",
+    description: "Learn Git & Github",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png",
+  },
+  {
+    id: 2,
+    title: "Hacktoberfest",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBICPHJ86kQ-y6p2-Z9K_ibEbYmVmo6Eda-w&usqp=CAU",
+  },
+  {
+    id: 3,
+    title: "Hacktoberfest",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBICPHJ86kQ-y6p2-Z9K_ibEbYmVmo6Eda-w&usqp=CAU",
+  },
+];
