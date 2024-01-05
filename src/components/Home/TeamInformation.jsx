7a39ddaed492a9121903851151e494a0d1ecc52c
Priyadarshani Kalbandhe <priyadarshanik07@gmail.com>
2024-01-05 15:32:35 +0530

    team

---
+++ src/components/Home/TeamInformation.jsx
@@ -0,0 +1,171 @@
+import kirtan from "../../assets/teammembers/kirtan.jpg";
+import devyani from "../../assets/teammembers/devyani.jpg";
+import priyadarshani from "../../assets/teammembers/priyadarshani.jpg";
+import jayesh from "../../assets/teammembers/jayesh.jpg";
+import yashodeep from "../../assets/teammembers/yashodeep.jpeg";
+import alfiya from "../../assets/teammembers/alfiya.jpg";
+import atharva from "../../assets/teammembers/atharva.jpg";
+import riya from "../../assets/teammembers/riya.jpg";
+import aditya from "../../assets/teammembers/aditya.jpg";
+import gaurav from "../../assets/teammembers/gaurav.jpg";
+import pranjal from "../../assets/teammembers/pranjal.jpg";
+import ankush from "../../assets/teammembers/ankush.jpg";
+import tejaswi from "../../assets/teammembers/tejaswi.jpg";
+import kalash from "../../assets/teammembers/kalash.jpg";
+import raj from "../../assets/teammembers/raj.png";
+import chirshmita from "../../assets/teammembers/chirshmita.jpg";
+import utkarsh from "../../assets/teammembers/utkarsh.jpg";
+import anurag from "../../assets/teammembers/anurag.png";
+import santosh from "../../assets/teammembers/santosh.jpg";
+import atharva2 from "../../assets/teammembers/atharva2.jpg";
+
+export const TeamInformation=[
+    {
+        id:"1",
+        img:kirtan,
+        name:"Kirtan Chandak",
+        position :"Team Lead",
+        type:"leads"
+    },
+    {
+        id:"2",
+        img:devyani,
+        name:"Devyani Chavhan",
+        position :"Co-Lead",
+        type:"leads"
+    },
+    {
+        id:"3",
+        img:priyadarshani,
+        name:"Priyadarshani Kalbandhe",
+        position :"Technical-Project Lead",
+        type:"tech"
+    },
+    {
+        id:"4",
+        img:jayesh,
+        name:"Jayesh Patil",
+        position :"Android Lead",
+        type:"tech"
+    },
+    {
+        id:"5",
+        img:yashodeep,
+        name:"Yashodeep",
+        position :"Team Member",
+        type:"tech"
+    },
+    {
+        id:"6",
+        img:alfiya,
+        name:"Alfia Attar",
+        position :"Team Member",
+        type:"tech"
+    },
+    {
+        id:"7",
+        img:atharva,
+        name:"Atharva Thakre",
+        position :"Team Member",
+        type:"tech"
+    },
+    {
+        id:"8",
+        img:riya,
+        name:"Riyadas Gupta",
+        position :"Team Member",
+        type:"tech"
+    },
+    {
+        id:"9",
+        img:aditya,
+        name:"Aditya Tiwari",
+        position :"Design Lead",
+        type:"design"
+    },
+    {
+        id:"10",
+        img:gaurav,
+        name:"Gaurav Nimbalkar",
+        position :"Design Co-Lead",
+        type:"design"
+    },
+    {
+        id:"11",
+        img:pranjal,
+        name:"Pranjal Chaudhary",
+        position :"PR Lead",
+        type:"pr"
+    },
+    {
+        id:"12",
+        img:ankush,
+        name:"Ankush Mishra",
+        position :"PR Co-Lead",
+        type:"pr"
+    },
+    {
+        id:"13",
+        img:tejaswi,
+        name:"Tejasvi Tiwari",
+        position :"Team Member",
+        type:"pr"
+    },
+    {
+        id:"14",
+        img:kalash,
+        name:"Kalash Singh",
+        position :"Team Member",
+        type:"pr"
+    },
+    {
+        id:"15",
+        img:raj,
+        name:"Raj Gupta",
+        position :"Event Lead",
+        type:"event"
+    },
+    {
+        id:"16",
+        img:raj,
+        name:"Priyanshu Tajne",
+        position :"Event Co-Lead",
+        type:"event"
+    },
+    {
+        id:"17",
+        img:chirshmita,
+        name:"Chrishmita Thakur",
+        position :"Team Member",
+        type:"event"
+    },
+    {
+        id:"18",
+        img:utkarsh,
+        name:"Utkarsh Shelke",
+        position :"Team Member",
+        type:"event"
+    },
+    {
+        id:"19",
+        img:anurag,
+        name:"Anurag",
+        position :"Content Lead",
+        type:"content"
+    },
+    {
+        id:"20",
+        img:santosh,
+        name:"Santosh Singh",
+        position :"Content Co-Lead",
+        type:"content"
+    },
+    {
+        id:"21",
+        img:atharva2,
+        name:"Atharva",
+        position :"Team Member",
+        type:"content"
+    },
+    
+];
